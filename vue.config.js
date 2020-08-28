3282e607364182be77ee4f9c13198667376db7ca
gcdeng <ericdenggc@gmail.com>
2020-08-28 11:39:29 +0800

    change title

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
       filename: "index.html",
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "Aaron Ruby Wedding Party"
+      title: "Aaron & Ruby Wedding Party"
     }
   }
 };
